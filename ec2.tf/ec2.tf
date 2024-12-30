6e1fa7e50f5227a7287fa9d13858fea9f4dacea9
HemanthKumarsankar <shemanth.devops@gmail.com>
2024-12-30 08:24:43 +0530

    ec2

---
+++ ec2.tf/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow_all_ssh-terraform" {
+  name  = "allow_sshh"
+  description " allow port number 22 for SSH access"
+
+# usaully we allow everything in egress
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] # allow for everyone
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
